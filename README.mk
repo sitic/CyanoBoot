bb3a17a8f4d1e7115932bf6efd9063accb232a7f
Jan Lebert <jan.lebert@online.de>
2012-03-07 23:47:53 +0100

    add inital README.mk

---
+++ README.mk
@@ -0,0 +1,19 @@
+CyanoBoot
+=========
+
+CyanoBoot is a 2nd stage Bootloader based on [u-boot](http://en.wikipedia.org/wiki/Das_U-Boot).
+It is in early development, see [Say hi to "CyanoBoot" -- a 2nd bootloader/w menu aka "ub2" - (WIP) @XDA-DEVELOPERS](http://forum.xda-developers.com/showthread.php?t=1522226) for details. 
+
+## Statements
+
+I\'m not the author of CyanoBoot, this is just a fork of http://forum.xda-developers.com/showthread.php?t=1522226 
+All credits goes to @fattire for his amazing work.
+Any help is appreciated.
+
+## Useful links for developing for Kindle Fire
+
+http://forum.xda-developers.com/showpost.php?p=23342025&postcount=208
+Linkfix for 3rd url:
+http://forum.xda-developers.com/showpost.php?p=19800610&postcount=74
+
+### Read README for more details
