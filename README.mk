cbf70d089fd9ca98a359bfe69c06e90908e01a5d
sitic <jan.lebert@online.de>
2012-03-07 23:54:40 +0100

    Update README.mk

--- README.mk
+++ README.mk
@@ -2,11 +2,11 @@ CyanoBoot
 =============
 
 CyanoBoot is a 2nd stage Bootloader based on [u-boot](http://en.wikipedia.org/wiki/Das_U-Boot).
-It is in early development, see [Say hi to "CyanoBoot" -- a 2nd bootloader/w menu aka "ub2" - (WIP) @XDA-DEVELOPERS](http://forum.xda-developers.com/showthread.php?t=1522226) for details. 
+It is in early development, see [Say hi to CyanoBoot -- a 2nd bootloader/w menu aka ub2 - WIP](http://forum.xda-developers.com/showthread.php?t=1522226) for details. 
 
 ## Statements
 
-I'm not the author of CyanoBoot, this is just a fork of http://forum.xda-developers.com/showthread.php?t=1522226 
+I\'m not the author of CyanoBoot, this is just a fork of http://forum.xda-developers.com/showthread.php?t=1522226 
 All credits goes to @fattire for his amazing work.
 Any help is appreciated.
 
